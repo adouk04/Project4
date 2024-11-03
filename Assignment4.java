abccb6559c1aa2007642771df8471a14e1d3342c
adouk <alexdouk253@gmail.com>
2024-11-03 01:30:16 -0700

    fixed driver method to skip invalid lines

--- Assignment4.java
+++ Assignment4.java
@@ -7,61 +7,94 @@ public class Assignment4 {
             Scanner input = new Scanner(new File("in4.txt"));
             PrintStream output = new PrintStream(new File("out4.txt"));
 
-            LinkedList<Shape> shapes = new LinkedList<>();
-
+            //everything for the original list
+            LinkedList<Shape> myList = new LinkedList<>();
             while (input.hasNextLine()) {
-                addShape(shapes, input.nextLine());
+                addShape(myList, input.nextLine());
             }
-
             output.println("Original List [unsorted]:");
-            for (Shape shape : shapes) {
+            for (Shape shape : myList) {
                 output.println(shape);
             }
+            ArrayList<Shape> newList = new ArrayList<Shape>();
 
+
+            //LinkedList<Shape> copyList = getOriginalList(input, myList);
+
+
+//            output.println("Original List[unsorted] of Size = " + myList.size() + ":");
+//            outputList(output, myList);
+//            Collections.sort(copyList);
+//            output.println("Copied List[unsorted] of Size = " + copyList.size() + ":");
+//            outputList(output, copyList);
+//            output.println("Original List[unsorted] of Size = " + myList.size() + ":");
+//            outputList(output, myList);
+
+            // closes all scanner and printstream
             console.close();
             input.close();
             output.close(); // Close output to finalize file writing
+            /**Sort the copyList and print out the myList,
+             // copyList, and then myList again by calling your output method 3 times.
+             //To further demonstrate the power and flexibility of inheritance,
+             // polymorphism, and abstract classes, instantiate
+             //your “copy” List as an ArrayList, have all methods that receive a
+             // List declare the parameter as List<Shape>.
+             //Because your LinkedList and an ArrayList both implement List,
+             // either type can be sent to your output method
+             //and used without changing any code.*/
 
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + e.getMessage());
         }
     }
+    public static void outputList(PrintStream output, LinkedList<Shape> myList) {
+        ;
+    }
+
+    public ArrayList<Shape> getOriginalList (LinkedList<Shape> myList) {
+        ArrayList<Shape> newList = new ArrayList<Shape>();
+        for (Shape element : myList) {
+            Shape s = element.copyShape();
+            newList.add(s);
+        }
+        return newList;
+    }
 
     public static void addShape (LinkedList<Shape> list, String line) {
         Scanner lineScanner = new Scanner(line);
         List<Double> values = new ArrayList<>();
-
-        while (lineScanner.hasNextDouble()) {
-            values.add(lineScanner.nextDouble());
+        while (lineScanner.hasNext()) {
+            if (lineScanner.hasNextDouble()) {
+                values.add(lineScanner.nextDouble());
+            }
+            else {
+                return;
+            }
         }
-
         if (values.size() == 1) {
-            list.add(new Circle(values.get(0)));
+            try {
+                list.add(new Circle(values.get(0)));
+            }
+            catch (IllegalArgumentException iAE){
+                System.out.println(iAE);
+            }
         }
         else if (values.size() == 2) {
-            list.add(new Rectangle(values.get(0), values.get(1)));
+            try {
+                list.add(new Rectangle(values.get(0), values.get(1)));
+            }
+            catch (IllegalArgumentException iAE){
+                System.out.println(iAE);
+            }
         }
         else if (values.size() == 3) {
-            list.add(new Triangle(values.get(0), values.get(1), values.get(2)));
+            try {
+                list.add(new Triangle(values.get(0), values.get(1), values.get(2)));
+            }
+            catch (IllegalArgumentException iAE){
+                System.out.println(iAE);
+            }
         }
     }
-
-    //unsorted original list
-
-    //method 1 circle
-    // read line
-    // see how many doubles are in that line
-    // if 1 then it's radius of a circle
-
-    //method 2 rectangle
-    // read line
-    // see how many doubles are in that line
-    // if 2 then it's the length and width of rectangle
-
-    //method 3 triangle
-    // read line
-    // see how many doubles are in that line
-    // if 3 then it's the sides of a triangle
-
-    //copied list sorted by the area
 }
